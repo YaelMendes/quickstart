a520932c7481890f683d1599cc05c6a187065891
mendesy <y.mendes@lombardodier.com>
2016-12-19 12:52:56 +0100

    share-7

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,9 +5,16 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
   getHeroes(): Promise<Hero[]> {
       return new Promise<Hero[]>(resolve =>
         setTimeout(resolve, 500)) // delay 2 seconds
         .then(() => Promise.resolve(HEROES));
   }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
 }
