bcdee7f1173f4023aaf199e4a5d868bfc99e4a9c
mendesy <y.mendes@lombardodier.com>
2016-12-19 15:40:31 +0100

    share-12

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -29,11 +29,6 @@ export class HeroService {
       .catch(this.handleError);
   }
 
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
-
   update(hero: Hero): Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
@@ -57,5 +52,10 @@ export class HeroService {
       .toPromise()
       .then(() => null)
       .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
